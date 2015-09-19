7094bc9178111acd8fa34181951f86be5cc1a086
Arzhan Kinzhalin <arzhan.i.kinzhalin@intel.com>
2015-09-18 17:21:49 -0700

    First iteration of refactoring.

--- client/wsrpcc.js
+++ client/wsrpcc.js
@@ -21,72 +21,53 @@
         }
         return result;
     }
-    
-    /** PEER EVENTS (from and to the remote peer) **/
-    
-    var _peerEventHandlers = {
-    };
-    
-    window.peer.on = function (event, callback) {
-        if (!_peerEventHandlers[event]) {
-            _peerEventHandlers[event] = [callback];
-        } else {
-            _peerEventHandlers[event].push(callback);
-        }
-    };
-    
-    
-    // this impl will be replaced on init
-    window.peer.dispatch = function (event, args) {
-        return;
-    };
-    
-    /** LIFECYCLE EVENTS (observe how this module does) **/
-    
-    var _E_OPEN     = "open",
-        _E_CLOSE    = "close",
-        _E_ERROR    = "error",
-        _E_PEER     = "peer",
-        _E_PUBLISH  = "publish";
-    
+
+    /* LIFECYCLE EVENTS (observe how this module does) */
+
+    var _E_OPEN             = "open",
+        _E_CLOSE            = "close",
+        _E_ERROR            = "error",
+    /* The events below are peer events (emitted and consumed on Peer instance) */
+        _E_CONNECT          = "connect",
+        _E_PUBLISH          = "publish",
+        _E_DISCONNECT       = "disconnect";
+
     var _eventHandlers = {
     };
-    
+
     function checkValidEvent(event) {
         switch (event) {
         case _E_CLOSE:
         case _E_ERROR:
         case _E_OPEN:
-        case _E_PEER:
-        case _E_PUBLISH:
             return true;
         default:
             return false;
         }
     }
-    
-    _wsrpc.on = function (event, callback) {
-        if (!checkValidEvent(event)) {
+
+    _wsrpc.on = function (e, callback) {
+        if (!checkValidEvent(e)) {
             return;
         }
-        if (!_eventHandlers[event]) {
-            _eventHandlers[event] = [callback];
+        if (!_eventHandlers[e]) {
+            _eventHandlers[e] = [callback];
         } else {
-            _eventHandlers[event].push(callback);
+            _eventHandlers[e].push(callback);
         }
     };
-    
-    function _dispatch(event, data) {
-        if (!checkValidEvent(event)) {
+
+    function _dispatch(e, data) {
+        if (!checkValidEvent(e)) {
             return;
         }
-        var handlers = _eventHandlers[event],
+        var handlers = _eventHandlers[e],
             i;
-        
+
         if (!handlers) {
             return;
         }
-        
+
         for (i = 0; i < handlers.length; i++) {
             handlers[i].apply(_wsrpc, data);
         }
@@ -96,193 +77,199 @@
     function _emptyHandler() {
         return;
     }
-    
-    var _calls = {};
-    
-    _wsrpc.publishAPI = function (def) {
+
+
+    function Peer(channel, ws) {
+        this.ws = ws;
+        this.channel = channel;
+        this._eventHandlers = {};
+        this._apiHandlers = {};
+        this.us = undefined;
+        this.them = undefined;
+        this.peer = {};
+    }
+
+    Peer.prototype.on = function (e, callback) {
+        if (!this._eventHandlers[event]) {
+            this._eventHandlers[event] = [callback];
+        } else {
+            this._eventHandlers[event].push(callback);
+        }
+    };
+
+    Peer.prototype._dispatch = function (e, args) {
+        var payload = {
+            type: "event",
+            id: this.channel,
+            us: this.us,
+            them: this.them,
+            name: e,
+            arguments: args
+        };
+
+        this.ws.send(JSON.stringify(payload));
+    };
+
+    Peer.prototype.publish = function (def) {
         var _if, /* own representation of the interface */
-            i;
+            i,
+            payload;
         /* reshape api to avoid sending functions. */
         _if = def;
-        if (!_conn.handlers) {
-            _conn.handlers = {};
-        }
         for (i = 0; i < def.api.length; i++) {
             if (!def.api[i].handler) {
                 _dispatch(_E_ERROR, ["No handler specified: " + JSON.stringify(def.api[i])]);
                 return;
             }
-            _conn.handlers[def.api[i].function] = def.api[i].handler;
+            this._apiHandlers[def.api[i].function] = def.api[i].handler;
             delete _if.api[i].handler;
         }
 
-        function pushAPI() {
-            var payload = {
-                type: "publish",
-                id: _conn.connection.id,
-                us: _conn.connection.us,
-                them: _conn.connection.them,
-                api: _if.api
-            };
-                
-            ws.send(JSON.stringify(payload));
-        }
-        
-        pushAPI();
+        payload = {
+            type: "publish",
+            id: this.channel,
+            us: this.us,
+            them: this.them,
+            api: _if.api
+        };
 
+        ws.send(JSON.stringify(payload));
     };
 
-    _wsrpc.init = function (port, id) {
-        
-        if (id) {
-            _conn.connection = {};
-            _conn.connection.id = id;
-        }
-        
-        ws = new WebSocket("ws://localhost:" + port + "/wsrpc");
-        
-        function emitEvent(event, args) {
-            var payload = {
-                type: "event",
-                id: _conn.connection.id,
-                us: _conn.connection.us,
-                them: _conn.connection.them,
-                name: event,
-                arguments: args
-            };
-            
-            ws.send(JSON.stringify(payload));
-        }
-        
-        function handlePublish(payload) {
-            var i;
-            
-            function makeFunction(api) {
-                return function () {
-                    var call,
-                        handleReturn = false,
-                        transmitArguments = Array.prototype.slice.call(arguments);
-                    if (arguments.length > 0 && typeof arguments[arguments.length - 1] === "function") {
-                        handleReturn = true;
-                        transmitArguments.pop();
-                    }
-                    if ((handleReturn && arguments.length !== api.arguments.length + 1) ||
-                            (!handleReturn && arguments.length !== api.arguments.length)) {
-                        return;
-                    }
-                    call = {
-                        type: "call",
-                        id: _conn.connection.id,
-                        us: _conn.connection.us,
-                        them: _conn.connection.them,
-                        fn: api.function,
-                        arguments: transmitArguments,
-                        trace: _getRandomID()
-                    };
-                    _calls[call.trace] = handleReturn ? arguments[arguments.length - 1] : _emptyHandler;
-                    ws.send(JSON.stringify(call));
-                };
-            }
-            
-            for (i = 0; i < payload.api.length; i++) {
-                console.log("Creating function: " + payload.api[i].function);
-                window.peer[payload.api[i].function] = makeFunction(payload.api[i]);
-            }
+    Peer.prototype._handlePublish = function (payload) {
+        var self,
+            i;
 
-            _dispatch(_E_PUBLISH, []);
-            
-            window.peer.ready = true;
-        }
-        
-        function handleCall(payload) {
-            var result = _conn.handlers[payload.fn].apply(null, payload.arguments),
-                rpayload = {
-                    type: "return",
-                    id: _conn.connection.id,
-                    us: _conn.connection.us,
-                    them: _conn.connection.them,
-                    trace: payload.trace,
-                    result: result
+        function makeFunction(api) {
+            return function () {
+                var call,
+                    handleReturn = false,
+                    transmitArguments = Array.prototype.slice.call(arguments);
+                if (arguments.length > 0 && typeof arguments[arguments.length - 1] === "function") {
+                    handleReturn = true;
+                    transmitArguments.pop();
+                }
+                if ((handleReturn && arguments.length !== api.arguments.length + 1) ||
+                        (!handleReturn && arguments.length !== api.arguments.length)) {
+                    return;
+                }
+                call = {
+                    type: "call",
+                    id: self.channel,
+                    us: self.us,
+                    them: self.them,
+                    fn: api.function,
+                    arguments: transmitArguments,
+                    trace: _getRandomID()
                 };
-            ws.send(JSON.stringify(rpayload));
+                _calls[call.trace] = handleReturn ? arguments[arguments.length - 1] : _emptyHandler;
+                self.ws.send(JSON.stringify(call));
+            };
         }
-        
-        function handleReturn(payload) {
-            var callback = _calls[payload.trace];
-            callback(payload.result);
+
+        for (i = 0; i < payload.api.length; i++) {
+            console.log("Creating function: " + payload.api[i].function);
+            this.peer[payload.api[i].function] = makeFunction(payload.api[i]);
         }
-        
-        function handleEvent(payload) {
-            var handlers = _peerEventHandlers[payload.name],
-                i;
-            
-            if (!handlers) {
-                return;
-            }
-            
-            for (i = 0; i < handlers.length; i++) {
-                handlers[i].apply(window.peer, payload.arguments);
-            }
-        }
-        
+
+        this._dispatch(_E_PUBLISH, []);
+
+        // FIXME: should not have "ready"...
+        this.peer.ready = true;
+    };
+
+    // FIXME: does not handle async handlers
+    Peer.prototype._handleCall = function (payload) {
+        var result = _conn.handlers[payload.fn].apply(null, payload.arguments),
+            rpayload = {
+                type: "return",
+                id: this.channel,
+                us: this.us,
+                them: this.them,
+                trace: payload.trace,
+                result: result
+            };
+        this.ws.send(JSON.stringify(rpayload));
+    };
+
+    Peer.prototype._handleReturn = function (payload) {
+        var callback = _calls[payload.trace];
+        callback(payload.result);
+    };
+
+
+    var _calls = {};
+
+    _wsrpc.init = function (port, id) {
+        var peer;
+
+        ws = new WebSocket("ws://localhost:" + port + "/wsrpc");
+
+        peer = new Peer(id, ws);
+
         ws.onopen = function () {
             _dispatch(_E_OPEN, []);
             var payload = {
                 type: "handshake",
-                id: _conn.connection.id
+                id: id
             };
             ws.send(JSON.stringify(payload));
         };
-        
-        ws.onmessage = function (e) {
-            var payload;
-            
-            try {
-                payload = JSON.parse(e.data);
-            } catch (err) {
-                console.error("Cannot parse payload " + e.data);
-                return;
-            }
-            
-            if (!payload.type) {
-                console.error("Payload has not type " + e.data);
-                return;
-            }
-            
-            switch (payload.type) {
-            case "handshake":
-                _conn.connection.us = payload.us;
-                _conn.connection.them = payload.them;
-                _dispatch(_E_PEER, [_conn.connection.them]);
-                window.peer.dispatch = emitEvent;
-                break;
-            case "publish":
-                handlePublish(payload);
-                break;
-            case "call":
-                handleCall(payload);
-                break;
-            case "return":
-                handleReturn(payload);
-                break;
-            case "event":
-                handleEvent(payload);
-                break;
-            default:
-                console.error("Payload type is not understood: " + payload.type);
-                return;
-            }
-        };
-        
+
+        /* This function generates a closure for this specific peer. */
+        function generateOnMessageHandler (p) {
+            return function (e) {
+                var payload;
+
+                try {
+                    payload = JSON.parse(e.data);
+                } catch (err) {
+                    console.error("Cannot parse payload " + e.data);
+                    return;
+                }
+
+                if (!payload.type) {
+                    console.error("Payload has not type " + e.data);
+                    return;
+                }
+
+                switch (payload.type) {
+                case "handshake":
+                    p.us = payload.us;
+                    p.them = payload.them;
+                    p._dispatch(_E_PEER, [p.them]);
+                    break;
+                case "publish":
+                    p.handlePublish(payload);
+                    break;
+                case "call":
+                    p.handleCall(payload);
+                    break;
+                case "return":
+                    p.handleReturn(payload);
+                    break;
+                case "event":
+                    p.handleEvent(payload);
+                    break;
+                default:
+                    console.error("Payload type is not understood: " + payload.type);
+                    return;
+                }
+            };
+        }
+
+        ws.onmessage = generateOnMessageHandler(peer);
+
+        // FIXME: onerror and onclose should be handler inside peer and emit events on the global
         ws.onerror = function (e) {
             _dispatch(_E_ERROR, ["WS error " + e]);
         };
-        
+
         ws.onclose = function (e) {
             _dispatch(_E_CLOSE, []);
         };
     };
-    
+
     window.wsrpc = _wsrpc;
 
 }());
