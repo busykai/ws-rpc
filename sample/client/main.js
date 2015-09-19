669b0669c2d197c9526a18471010cb08f6ad4c6f
Arzhan Kinzhalin <arzhan.i.kinzhalin@intel.com>
2015-09-18 17:40:41 -0700

    Re-do the sample client.

--- sample/client/main.js
+++ sample/client/main.js
@@ -10,11 +10,11 @@
     function userAgent() {
         return navigator.userAgent;
     }
-    
+
     function setContent(val) {
         document.getElementById("input").value = val;
     }
-    
+
     function getPeerInterface() {
         return {
             api: [
@@ -31,67 +31,69 @@
             ]
         };
     }
-    
+
     var eventCount = 0;
 
     window.addEventListener("load", function () {
+
+        var peer;
 
         var uaButton = document.getElementById("call"),
             input = document.getElementById("input"),
             getContentButton = document.getElementById("getcontent"),
             status = document.getElementById("status"),
             generateEvent = document.getElementById("haha");
-        
-        uaButton.addEventListener("click", function () {
-            if (peer.ready) {
-                peer.userAgent(function (result) {
-                    var pre = document.getElementById("result");
-                    pre.innerHTML = result;
-                });
-            }
-        });
-        
+
         var port = window.location.search.substring(1);
         try {
             port = parseInt(port, 10);
         } catch (e) {
             console.error("Cannot parse port: " + port);
         }
-        getContentButton.addEventListener("click", function () {
-            if (peer.ready) {
-                peer.setContent(input.value);
-            }
-        });
-        
-        generateEvent.addEventListener("click", function () {
-            if (peer.ready) {
-                peer.dispatch("haha");
-            }
-        });
-        
+
+        // initialize WS-RPC with the published interface
+        peer = wsrpc.init(port, "channel1");
+
         peer.on("haha", function () {
             var pre = document.getElementById("event");
             pre.innerHTML += "Event " + (eventCount++) + ": 'haha'\n";
         });
-        
+
+        peer.on("connect", function (id) {
+            status.innerHTML += "Peer connected (id " + id + ").\n";
+            peer.publish(getPeerInterface());
+        });
+
+        peer.on("publish", function () {
+            uaButton.addEventListener("click", function () {
+                peer.userAgent(function (result) {
+                    var pre = document.getElementById("result");
+                    pre.innerHTML = result;
+                });
+            });
+
+            getContentButton.addEventListener("click", function () {
+                peer.setContent(input.value);
+            });
+
+            generateEvent.addEventListener("click", function () {
+                peer.dispatch("haha");
+            });
+        });
+
         wsrpc.on("open", function () {
             status.innerHTML += "Connected to the proxy!\n";
         });
-        wsrpc.on("peer", function (id) {
-            status.innerHTML += "Peer connected (id " + id + ").\n";
-            wsrpc.publishAPI(getPeerInterface());
-        });
+
         wsrpc.on("close", function (id) {
             status.style.backgroundColor = "#c95656";
             status.innerHTML += "Connection to the proxy was closed.";
         });
+
         wsrpc.on("error", function (id) {
             status.innerHTML += "Error occured.\n";
             status.style.backgroundColor = "#c95656";
         });
-        
-        // initialize WS-RPC with the published interface
-        wsrpc.init(port, "channel1");
 
     });
 }());
